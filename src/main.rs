b522288159b47c21769c8a713c73920e6d527d03
rey256 <medvedosandr@gmail.com>
2023-10-18 02:11:37 +0300

    work

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
-use std::sync::Mutex;
+use axum::{extract::State, routing::post, Json, Router};
+use hound::WavReader;
+use std::path::PathBuf;
+use std::process;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use vosk::{Model, Recognizer};
+use ytd_rs::{Arg, YoutubeDL};
 
-use axum::Json;
+#[derive(Clone)]
+struct AppData {
+    temp_counter: Arc<Mutex<u32>>,
+}
 
-// #[get("/")]
-// async fn hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hello world!")
-// }
+#[derive(serde::Deserialize)]
+struct Ytdlp {
+    url: String,
+}
 
-// #[post("/echo")]
-// async fn echo(req_body: String) -> impl Responder {
-//     HttpResponse::Ok().body(req_body)
-// }
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/ffmpeg", post(ffmpeg_page))
+        .route("/yt", post(yt_dlp))
+        .route("/vosk", post(vosk_page))
+        .route("/full", post(full_cycle))
+        .with_state(AppData {
+            temp_counter: Arc::new(Mutex::new(0)),
+        });
 
-// #[get("/json")]
-// async fn json() -> impl Responder {
-//     HttpResponse::Ok().json(Ytdlp {
-//         url: "https://youtu.be/dQw4w9WgXcQ".to_string(),
-//     })
-// }
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
-// #[get("/hi")]
-// async fn hi() -> impl Responder {
-//     HttpResponse::Ok().body("hi")
-// }
-
-#[post("/ffmpeg")]
-async fn ffmpeg_page(path: Json<Ytdlp>) -> impl Responder {
-    use std::process;
-
+async fn ffmpeg_page(path: Json<Ytdlp>) -> String {
     println!("got ffmpeg");
     println!("{:?}", std::env::current_dir().unwrap().to_str().unwrap());
+
     let input_path = format!("./downloads/{}", path.url);
-    let name_without_ext = path.url.split('.').into_iter().next().unwrap();
-    let output_name = format!("./ffmpeg/{}.wav", name_without_ext);
+    let name_without_ext = path.url.split('.').next().unwrap();
+    let output_name = format!("./ffmpeg/{name_without_ext}.wav");
 
     let mut ffmpeg = process::Command::new("ffmpeg")
         .args(vec![
@@ -52,38 +60,27 @@ async fn ffmpeg_page(path: Json<Ytdlp>) -> impl Responder {
 
     // println!("{}", String::from_utf8(ffmpeg.stdout).unwrap());
 
-    HttpResponse::Ok().body(format!("{}.wav", output_name))
+    format!("{output_name}.wav")
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
-struct Ytdlp {
-    url: String,
-}
-
-#[post("/yt")]
-async fn yt_dlp(url: web::Json<Ytdlp>, data: web::Data<AppData>) -> impl Responder {
-    use std::path::PathBuf;
-    use ytd_rs::{Arg, YoutubeDL};
-
+async fn yt_dlp(State(data): State<AppData>, url: Json<Ytdlp>) -> String {
     println!("got reqwest {}", url.url);
-    let output_name = format!("temp{}", data.temp_counter.lock().unwrap().to_string());
-    *data.temp_counter.lock().unwrap() += 1;
+    let output_name = format!("temp{}", data.temp_counter.lock().await);
+    *data.temp_counter.lock().await += 1;
 
     let args = vec![
         Arg::new("--quiet"),
-        Arg::new_with_arg("--output", format!("{}.%(ext)s", output_name).as_str()),
+        Arg::new_with_arg("--output", format!("{output_name}.%(ext)s").as_str()),
     ];
     // let link = "https://www.youtube.com/watch?v=uTO0KnDsVH0";
     let path = PathBuf::from("./downloads");
-    let ytd = match YoutubeDL::new(&path, args, &url.url) {
-        Ok(res) => res,
-        Err(_) => return HttpResponse::Conflict().body(()),
+    let Ok(ytd) = YoutubeDL::new(&path, args, &url.url) else {
+        return String::new();
     };
 
     // start download
-    let download = match ytd.download() {
-        Ok(res) => res,
-        Err(_) => return HttpResponse::Conflict().body(()),
+    let Ok(download) = ytd.download() else {
+        return String::new();
     };
 
     // print out the download path
@@ -92,19 +89,15 @@ async fn yt_dlp(url: web::Json<Ytdlp>, data: web::Data<AppData>) -> impl Respond
         download.output_dir().to_string_lossy(),
         output_name
     );
-    HttpResponse::Ok().body(format!("{}.webm", output_name))
+
+    format!("{output_name}.webm")
 }
 
-#[post("/vosk")]
-async fn vosk_page(url: web::Json<Ytdlp>) -> impl Responder {
-    let res = vosk(url.url.clone());
-    HttpResponse::Ok().body(res)
+async fn vosk_page(url: Json<Ytdlp>) -> String {
+    vosk(url.url.clone())
 }
 
 fn vosk(wav_path: String) -> String {
-    use hound::WavReader;
-    use vosk::{Model, Recognizer};
-
     // let model_path = "/home/zbykovd/Downloads/vosk-model-small-ru-0.22";
     let model_path = "/home/zbykovd/Downloads/vosk-model-small-en-us-0.15/";
     // let model_path = "/home/zbykovd/Downloads/vosk-model-en-us-0.22/";
@@ -138,31 +131,25 @@ fn vosk(wav_path: String) -> String {
     recognizer.final_result().single().unwrap().text.to_string()
 }
 
-#[post("/full")]
-async fn full_cycle(url: web::Json<Ytdlp>, data: web::Data<AppData>) -> impl Responder {
+async fn full_cycle(State(data): State<AppData>, url: Json<Ytdlp>) -> String {
     // YT_DLP
-    use std::path::PathBuf;
-    use ytd_rs::{Arg, YoutubeDL};
-
     println!("got reqwest {}", url.url);
-    let output_name = format!("temp{}", data.temp_counter.lock().unwrap().to_string());
-    *data.temp_counter.lock().unwrap() += 1;
+    let output_name = format!("temp{}", data.temp_counter.lock().await);
+    *data.temp_counter.lock().await += 1;
 
     let args = vec![
         Arg::new("--quiet"),
-        Arg::new_with_arg("--output", format!("{}.%(ext)s", output_name).as_str()),
+        Arg::new_with_arg("--output", format!("{output_name}.%(ext)s").as_str()),
     ];
     // let link = "https://www.youtube.com/watch?v=uTO0KnDsVH0";
     let path = PathBuf::from("./downloads");
-    let ytd = match YoutubeDL::new(&path, args, &url.url) {
-        Ok(res) => res,
-        Err(_) => return HttpResponse::Conflict().body(()),
+    let Ok(ytd) = YoutubeDL::new(&path, args, &url.url) else {
+        return String::new();
     };
 
     // start download
-    let download = match ytd.download() {
-        Ok(res) => res,
-        Err(_) => return HttpResponse::Conflict().body(()),
+    let Ok(download) = ytd.download() else {
+        return String::new();
     };
 
     // print out the download path
@@ -173,13 +160,11 @@ async fn full_cycle(url: web::Json<Ytdlp>, data: web::Data<AppData>) -> impl Res
     );
 
     // FFMPEG
-    use std::process;
-
     println!("got ffmpeg");
     println!("{:?}", std::env::current_dir().unwrap().to_str().unwrap());
-    let input_path = format!("./downloads/{}.webm", output_name);
-    let name_without_ext = output_name.split('.').into_iter().next().unwrap();
-    let output_path_and_name = format!("./ffmpeg/{}.wav", name_without_ext);
+    let input_path = format!("./downloads/{output_name}.webm");
+    let name_without_ext = output_name.split('.').next().unwrap();
+    let output_path_and_name = format!("./ffmpeg/{name_without_ext}.wav");
 
     let mut ffmpeg = process::Command::new("ffmpeg")
         .args(vec![
@@ -194,37 +179,12 @@ async fn full_cycle(url: web::Json<Ytdlp>, data: web::Data<AppData>) -> impl Res
         ])
         .spawn()
         .unwrap();
+
     ffmpeg.wait().unwrap();
+
     println!("done ffmpeg");
-    println!("{}", output_path_and_name);
+    println!("{output_path_and_name}");
 
     // VOSK
-    let res = vosk(output_path_and_name);
-
-    HttpResponse::Ok().body(res)
-}
-
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
-
-struct AppData {
-    temp_counter: Mutex<u32>,
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppData {
-                temp_counter: Mutex::new(0),
-            }))
-            .service(yt_dlp)
-            .service(ffmpeg_page)
-            .service(vosk_page)
-            .service(full_cycle)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    vosk(output_path_and_name)
 }
