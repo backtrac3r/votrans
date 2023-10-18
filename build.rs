2a4706704ca83378abcd4db2146ce363206281ae
rey256 <medvedosandr@gmail.com>
2023-10-19 00:00:11 +0300

    work

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
-// build.rs
-
 fn main() {
-    println!("cargo:include=./lib/vosk");
-    println!("cargo:rustc-link-search=./lib/vosk");
-
-    println!("cargo:rustc-env=LD_LIBRARY_PATH=./lib/vosk");
+    println!("cargo:include=./lib/linux_vosk");
+    println!("cargo:rustc-link-search=./lib/linux_vosk");
 }
