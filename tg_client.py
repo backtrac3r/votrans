72680fb290b1f4da5b88764f63d926e7fc27c44c
backtrac3r <medvedosandr@gmail.com>
2023-11-06 15:31:15 +0300

    .

--- tg_client.py
+++ tg_client.py
@@ -1,19 +1,19 @@
+import asyncio
 import argparse
-from telethon import TelegramClient
+from pyrogram import Client
 
-parser = argparse.ArgumentParser(description='Optional app description')
+parser = argparse.ArgumentParser(description="Optional app description")
 
-parser.add_argument('--file_id', type=str, help='file_id')
-parser.add_argument('--api_id', type=int, help='api_id')
-parser.add_argument('--api_hash', type=str, help='api_hash')
+parser.add_argument("--file_id", type=str, help="file_id")
+parser.add_argument("--api_id", type=int, help="api_id")
+parser.add_argument("--api_hash", type=str, help="api_hash")
 
 args = parser.parse_args()
 
-client = TelegramClient('anon', args.api_id, args.api_hash)
-
 async def main():
-    file_bytes = await client.download_media(args.file_id, file=bytes)
-    print(file_bytes)
+    async with Client("anon", args.api_id, args.api_hash, max_concurrent_transmissions=2) as client:
+        path = await client.download_media(args.file_id)
+        print(path)
 
-with client:
-    client.loop.run_until_complete(main())
+
+asyncio.run(main())
