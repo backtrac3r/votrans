a91507c52a0ae2a2d7d0e53555c64585b74bb21f
backtrac3r <medvedosandr@gmail.com>
2023-11-08 10:31:13 +0300

    .

--- tg_client.py
+++ tg_client.py
@@ -4,16 +4,16 @@ from pyrogram import Client
 
 parser = argparse.ArgumentParser(description="Optional app description")
 
-parser.add_argument("--file_id", type=str, help="file_id")
 parser.add_argument("--api_id", type=int, help="api_id")
 parser.add_argument("--api_hash", type=str, help="api_hash")
 
 args = parser.parse_args()
 
-async def main():
-    async with Client("anon", args.api_id, args.api_hash, max_concurrent_transmissions=2) as client:
-        path = await client.download_media(args.file_id)
-        print(path)
+app = Client("anon", args.api_id, args.api_hash, max_concurrent_transmissions=3)
 
+@app.on_message()
+async def handler(client, message):
+    await message.download(file_name="temp/")
+    await message.reply("complete")
 
-asyncio.run(main())
+app.run()
